33ad7b2e4a160a0f2554065104affea64fb467bd
Albin Söderqvist <albin.soderqvist@hostmobility.com>
2021-10-12 10:30:01 +0200

    Use Linux coding style
    Harmonize indentation, delete trailing whitespace, use snake case for
    functions and do similar adjustments.

--- asix_eepromtool.c
+++ asix_eepromtool.c
@@ -46,9 +46,9 @@ For more information, please refer to <http://unlicense.org/>
 #include <endian.h>
 #include <getopt.h>
 
-#define ASIX_CMD_READ_EEPROM		0x0b 
-#define ASIX_CMD_WRITE_EEPROM		0x0c 
-#define ASIX_CMD_WRITE_EEPROM_EN	0x0d 
+#define ASIX_CMD_READ_EEPROM		0x0b
+#define ASIX_CMD_WRITE_EEPROM		0x0c
+#define ASIX_CMD_WRITE_EEPROM_EN	0x0d
 #define ASIX_CMD_WRITE_EEPROM_DIS	0x0e
 
 static libusb_device *dev, **devs;
@@ -167,6 +167,7 @@ static int open_device(int bus, int devnum, unsigned int vid, unsigned int pid)
 	} else {
 		printf("\n");
 	}
+
 	libusb_detach_kernel_driver(device, 0);
 	status = libusb_claim_interface(device, 0);
 	if (status != LIBUSB_SUCCESS) {
@@ -181,59 +182,58 @@ static int open_device(int bus, int devnum, unsigned int vid, unsigned int pid)
 
 static int asix_read(uint8_t cmd, uint16_t value, uint16_t index, uint16_t size, void *data)
 {
-
 	return libusb_control_transfer(device,
-					LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
-					cmd,
-					value, 
-					index,
-					data, 
-					size, 
-					100);
+				       LIBUSB_ENDPOINT_IN | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
+				       cmd,
+				       value,
+				       index,
+				       data,
+				       size,
+				       100);
 }
 
 static int asix_write(uint8_t cmd, uint16_t value, uint16_t index, uint16_t size, void *data)
 {
-
 	return libusb_control_transfer(device,
-					LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
-					cmd,
-					value, 
-					index,
-					data, 
-					size, 
-					100);
+				       LIBUSB_ENDPOINT_OUT | LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
+				       cmd,
+				       value,
+				       index,
+				       data,
+				       size,
+				       100);
 }
 
-int read_eeprom(int rsize, uint16_t* buf) {
-
+int read_eeprom(int rsize, uint16_t* buf)
+{
 	int i, retval = 0;
-	uint16_t tmp16;	
+	uint16_t tmp16;
 
 	for (i = 0; i < rsize/2; i++) {
 		retval = asix_read(ASIX_CMD_READ_EEPROM, i, 0, 2, &tmp16);
 
-		if(retval < 0) {
+		if (retval < 0) {
 			return retval;
 		}
 
 		*(buf + i) = be16toh(tmp16);
 	}
+
 	return retval;
-	
 }
 
-int write_eeprom(int wsize, uint16_t* buf) {
+int write_eeprom(int wsize, uint16_t* buf)
+{
 	int i, retval;
 
 	retval = asix_write(ASIX_CMD_WRITE_EEPROM_EN, 0, 0, 0, NULL);
 	if (retval < 0) {
-		 return retval;
+		return retval;
 	}
 
 	sleep(1);
 
-	for (i = 0; i < wsize/2; i++){
+	for (i = 0; i < wsize/2; i++) {
 		retval = asix_write(ASIX_CMD_WRITE_EEPROM, i, htobe16(*(buf + i)), 0, NULL);
 
 		if (retval < 0) {
@@ -244,38 +244,39 @@ int write_eeprom(int wsize, uint16_t* buf) {
 
 	retval = asix_write(ASIX_CMD_WRITE_EEPROM_DIS, 0, 0, 0, NULL);
 	if (retval < 0) {
-			return retval;
+		return retval;
 	}
 
 	return retval;
 }
 
-void printHeader() {
-
-	  printf("------------------------------------\n");
-	  printf("        asix_eepromtool\n");
- 	  printf("------------------------------------\n");
-}
-void printUsage() {
-	  printHeader();
-	  printf("options:\n");
-	  printf("--device=<vid:pid> -d  <vid:pid>        =   vid and pid of device in hex eg. 0b95:772b\n");
-	  printf("--bus=<bus> -b  <bus>                   =   (optional) bus number, e.g. 2 or -1 to select the last\n");
-	  printf("--device-number=<n> -n  <n>             =   (optional) device number, e.g. 4 or -1 to select the last\n");
-	  printf("--read=<file> ,  -r <file>              =   save the eeprom contents to <file>\n");
-	  printf("--write=<file> ,   -w <file>            =   write <file> to eeprom\n");
-	  printf("--size=<# of bytes> , -s <# of bytes>   =   size of eeprom in bytes (e.g. 256 or 512)\n");
-	  printf("\n");
-	  printf("example:\n");
-	  printf("asix_eepromtool -d 0b95:772b -r eep.bin -s 256\n");
-	  printf("asix_eepromtool -d 0b95:772b -b 2 -n 10 -r eep.bin -s 256\n");
-	  printf("\n");
-	  printf("ps. Run this tool as root\n");
+void print_header()
+{
+	printf("------------------------------------\n");
+	printf("        asix_eepromtool\n");
+	printf("------------------------------------\n");
 }
 
+void print_usage()
+{
+	print_header();
+	printf("options:\n");
+	printf("--device=<vid:pid> -d  <vid:pid>        =   vid and pid of device in hex eg. 0b95:772b\n");
+	printf("--bus=<bus> -b  <bus>                   =   (optional) bus number, e.g. 2 or -1 to select the last\n");
+	printf("--device-number=<device> -n  <n>        =   (optional) device number, e.g. 4 or -1 to select the last\n");
+	printf("--read=<file> ,  -r <file>              =   save the eeprom contents to <file>\n");
+	printf("--write=<file> ,   -w <file>            =   write <file> to eeprom\n");
+	printf("--size=<# of bytes> , -s <# of bytes>   =   size of eeprom in bytes (e.g. 256 or 512)\n");
+	printf("\n");
+	printf("example:\n");
+	printf("asix_eepromtool -d 0b95:772b -r eep.bin -s 256\n");
+	printf("asix_eepromtool -d 0b95:772b -b 2 -n 10 -r eep.bin -s 256\n");
+	printf("\n");
+	printf("ps. Run this tool as root\n");
+}
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int status;
 	uint8_t eepbuf[65536];
 	int c;
@@ -288,93 +289,87 @@ int main(int argc, char **argv) {
 	FILE *readFile = NULL;
 	FILE *writeFile = NULL;
 
-	if (argc==1) {
-		 printUsage();
-		 exit(1);
+	if (argc == 1) {
+		print_usage();
+		exit(1);
 	}
 
-	static struct option long_options[] =
-        {
-          {"device",  required_argument, 0, 'd'},
-          {"bus",  required_argument, 0, 'b'},
-          {"device-number",  required_argument, 0, 'n'},
-          {"read",  required_argument, 0, 'r'},
-          {"write",    required_argument, 0, 'w'},
-          {"size",    required_argument, 0, 's'},
-	          {0, 0, 0, 0}
-       	};
+	static struct option long_options[] = {
+			{"device", required_argument, 0, 'd'},
+			{"bus", required_argument, 0, 'b'},
+			{"device-number", required_argument, 0, 'n'},
+			{"read", required_argument, 0, 'r'},
+			{"write", required_argument, 0, 'w'},
+			{"size", required_argument, 0, 's'},
+			{0, 0, 0, 0}
+	};
 
-	while (1)
-	{
+	while (1) {
+		int option_index = 0;
 
-	      int option_index = 0;
+		c = getopt_long (argc, argv, "d:b:n:r:w:s:",
+				 long_options, &option_index);
 
-	      c = getopt_long (argc, argv, "d:b:n:r:w:s:",
-                       long_options, &option_index);
+		if (c == -1) {
+			break;
+		}
 
-	      if (c == -1)
-	        break;
-
-	      switch (c)
-	        {
+		switch (c) {
 	        case 0:
-	          if (long_options[option_index].flag != 0)
-	            break;
-
+			if (long_options[option_index].flag != 0) {
+				break;
+			}
 		case 'b':
 			bus = atoi(optarg);
 			if (bus < -1) {
-				printUsage();
+				print_usage();
 				exit(1);
 			}
 			break;
-
 		case 'n':
 			devnum = atoi(optarg);
 			if (devnum < -1) {
-				printUsage();
+				print_usage();
 				exit(1);
 			}
 			break;
-
         	case 'd':
 			tmp = strtok(optarg,":");
 			vid = strtoul(tmp,NULL,16);
 			tmp = strtok(NULL,":");
-			if (tmp != NULL)
+			if (tmp != NULL) {
 				pid=strtoul(tmp,NULL,16);
-	        break;
-
+			}
+			break;
         	case 'r':
-			readFile = fopen(optarg,"wb");				          
-	        break;
-
+			readFile = fopen(optarg,"wb");
+			break;
 	        case 'w':
 	        	writeFile = fopen(optarg,"rb");
-	        break;
-		case 's':		
+			break;
+		case 's':
 			eepsize = strtoul(optarg,NULL,10);
-		break;
+			break;
 	        case '?':
-			printUsage();
+			print_usage();
 			exit(0);
-	          break;
+			break;
 	        default:
-		  abort;
+			abort;
 	        }
-    }
+	}
 
-	printHeader();
-	if ((vid==0) | (pid==0)) {
+	print_header();
+	if ((vid == 0) | (pid == 0)) {
 		printf("Device VID:PID missing or wrong format\n");
 		exit(0);
-	} 
-	if (eepsize ==0) {
+	}
+	if (eepsize == 0) {
 		printf("EEPROM size not specified\n");
 		exit(0);
-	} 
+	}
 
-	if ((readFile ==NULL)&&(writeFile==NULL)) {
+	if ((readFile == NULL) && (writeFile == NULL)) {
 		printf("Read/write filename not specified or file open error\n");
 		exit(0);
 	}
@@ -384,41 +379,39 @@ int main(int argc, char **argv) {
 		exit(0);
 	}
 
-
-        printf("Device is %04X:%04X\n",vid,pid);
-	printf("EEPROM is %d bytes\n",eepsize);
+        printf("Device is %04X:%04X\n", vid, pid);
+	printf("EEPROM is %d bytes\n", eepsize);
 
 	if (bus && devnum) {
 		printf("Bus %d, device %d\n", bus, devnum);
 	}
 
-	status = open_device(bus,devnum,vid,pid);
+	status = open_device(bus, devnum, vid, pid);
 
-	if (status!=0) {
+	if (status != 0) {
 		printf("Device open error %d\n",status);
 		exit(0);
 	} else {
 		printf("Device opened\n");
 	}
 
-	if (readFile !=NULL) {
+	if (readFile != NULL) {
 		printf("Reading...\n");
-		status = read_eeprom(eepsize,(uint16_t*)&eepbuf);
-		fwrite(&eepbuf,1,eepsize,readFile);
+		status = read_eeprom(eepsize, (uint16_t*)&eepbuf);
+		fwrite(&eepbuf, 1, eepsize, readFile);
 		fclose(readFile);
-
 	}
 
 	if (writeFile != NULL) {
 		printf("Writing...\n");
-		fread(&eepbuf,1,eepsize,writeFile);
-		status = write_eeprom(eepsize,(uint16_t*)&eepbuf);
+		fread(&eepbuf, 1, eepsize, writeFile);
+		status = write_eeprom(eepsize, (uint16_t*)&eepbuf);
         	fclose(writeFile);
-		
 	}
-	if (status <0) {
-		printf("Error %d\n",status);
+
+	if (status < 0) {
+		printf("Error %d\n", status);
 	} else {
-		printf("Done.\n");	
+		printf("Done.\n");
 	}
 }
